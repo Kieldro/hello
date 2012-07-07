fed7ea81b8d992999e6e30b5f3ee6e02581180fe
Ian Buitrago <kieldro@gmail.com>
2012-07-07 16:06:23 -0500

    some changes.

--- cpp/scratch.cpp
+++ cpp/scratch.cpp
@@ -1,10 +1,22 @@
+/*
+g++ scratch.cpp;if [ $? -eq 0 ];then ./a.out;fi
+*/
+
 #include <iostream>
 #include <cstdio>
-using namespace std;
+
+//using namespace std;
+using std::equal;
+using std::cout;
+using std::endl;
+
 int main (){
-	string s = "oioijoij";
-	std::cout << "c: " <<unsigned(0) << std::endl;
-	std::printf("literal %s\n", s.c_str() );
+	int x = 2147483647;		// x^31-1
+	 
+	cout << "x: " << sizeof(x) << endl;
+	
+	cout << "x: " << x << endl;
+	//std::printf("literal %s\n", s.c_str() );
 	
 	return 0;
 }
